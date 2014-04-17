a44773488631880366e0c454839033c82431fd3e
Chris Boursnell <cboursnell@gmail.com>
2014-04-17 13:57:21 +0100

    making database and running blast

--- lib/crb-blast.rb
+++ lib/crb-blast.rb
@@ -1,6 +1,154 @@
-class CRB_Blast
-  def self.hi
-    puts "hello world"
+class Bio::FastaFormat
+  def isNucl?
+    Bio::Sequence.guess(self, 0.9, 1000) == Bio::Sequence::NA
   end
+
+  def isProt?
+    Bio::Sequence.guess(self, 0.9, 1000) == Bio::Sequence::AA
+  end
+end
+
+class CRB_Blast
+
+  include Which
+
+
+
+  def initialize query, target
+    @query = query
+    @target = target
+    @working_dir = File.expand_path(File.dirname(query)) # no trailing /
+    @makedb_path = which('makeblastdb')
+    raise 'makeblastdb was not in the PATH' if @makedb_path.empty?
+    @blastn_path = which('blastn')
+    raise 'blastn was not in the PATH' if @blastn_path.empty?
+    @tblastn_path = which('tblastn')
+    raise 'tblastn was not in the PATH' if @tblastn_path.empty?
+    @blastx_path = which('blastx')
+    raise 'blastx was not in the PATH' if @blastx_path.empty?
+    @makedb_path = @makedb_path.first
+    @blastn_path = @blastn_path.first
+    @tblastn_path = @tblastn_path.first
+    @blastx_path = @blastx_path.first
+  end
+
+  #
+  # makes a blast database from the query and the target
+  #
+  def makedb
+    # check if the query is a nucleotide sequence
+    query_file = Bio::FastaFormat.open(@query)
+    entry = query_file.next
+    raise "Query sequence looks like it's not nucleotide" if !entry.isNucl?
+    # check if the target is a nucl or prot seq
+    target_file = Bio::FastaFormat.open(@target)
+    entry = target_file.next
+    @target_is_prot = entry.isProt?
+    # construct the output database names
+    @query_name = File.basename(opts.query).split('.')[0..-2].join('.')
+    @target_name = File.basename(opts.target).split('.')[0..-2].join('.')
+
+    # check if the databases already exist in @working_dir
+    make_query_db_cmd = "#{makedb_path} -in #{@query}"
+    make_query_db_cmd << " -dbtype nucl -title #{query_name} -out #{query_name}"
+    if !File.exists?("#{@working_dir}/#{query_name}.nin")
+      `#{make_query_db_cmd}`
+    end
+
+    make_target_db_cmd = "#{makedb_path} -in #{@target}"
+    make_target_db_cmd << " -dbtype nucl " if !@target_is_prot
+    make_target_db_cmd << " -dbtype prot " if @target_is_prot
+    make_target_db_cmd << " -title #{target_name} -out #{target_name}"
+
+    db_target = "#{target_name}.nsq" if !@target_is_prot
+    db_target = "#{target_name}.psq" if @target_is_prot
+    if !File.exists?("#{db_target}")
+      `#{make_target_db_cmd}`
+    end
+    @databases = true
+  end
+
+  def run_blast threads=1
+    if @databases
+      @output1 = "#{query_name}_into_#{target_name}.1.blast"
+      @output2 = "#{target_name}_into_#{query_name}.2.blast"
+      cmd1=""
+      cmd2=""
+      if @target_is_prot
+        cmd1 << "#{@blastx_path} "
+        cmd2 << "#{@tblastn_path} "
+      else
+        cmd1 << "#{@blastn_path} "
+        cmd2 << "#{@blastn_path} "       
+      end
+      cmd1 << " -query #{@query} -db #{@target_name} "
+      cmd1 << " -out #{@output1} -evalue 1e-5 -outfmt 6 "
+      cmd1 << " -num_descriptions 50 -num_alignments 50 "
+      cmd1 << " -num_threads #{threads}"
+      cmd2 << " -query #{@target} -db #{@query_name} "
+      cmd2 << " -out #{@output2} -evalue 1e-5 -outfmt 6 "
+      cmd2 << " -num_descriptions 50 -num_alignments 50 "
+      cmd2 << " -num_threads #{threads}" 
+
+      if !File.exists?("#{@output1}")
+        `#{cmd1}`
+      end
+      if !File.exists?("#{@output2}")
+        `#{cmd2}`
+      end
+    end
+  end
+
+  def load_outputs
+    @query_results = Hash.new
+    @target_results = Hash.new
+    if File.exists?("#{@output1}") and File.exists?("#{@output2}")
+      File.open("#{@output2}").each_line do |line|
+        cols = line.chomp.split("\t")
+        hit = Hit.new(cols)
+        query_results[hit.query] = [] if !query_results.has_key?(hit.query)
+        query_results[hit.query] << hit
+      end
+      File.open("#{@output2}").each_line do |line|
+        cols = line.chomp.split("\t")
+        hit = Hit.new(cols)
+        query_results[hit.query] = [] if !query_results.has_key?(hit.query)
+        query_results[hit.query] << hit
+      end
+    else
+      raise "need to run blast first"
+    end
+  end
+
+  def find_reciprocals
+    reciprocals = Hash.new
+    missed = Hash.new
+    fitting = Hash.new
+    evalues = []
+    missed_evalues = []
+    @query_results.each_pair do |query_id, list_of_hits|
+      # as the results are sorted the best one is at the top
+      best_hit_1 = list_of_hits[0]
+      if @target_results.has_key?(best_hit_1.target)
+        list_of_hits_2 = target_results[best_hit_1.target]
+        best_hit_2 = list_of_hits_2[0]
+        e = best_hit_2.evalue.to_f
+        e = 1e-200 if e==0
+        e = -Math.log10(e)
+        if best_hit_2.target == query_id # is a reciprocal hit
+          reciprocals[best_hit_1.query] = best_hit_1
+          longest = best_hit_1.alnlen  if best_hit_1.alnlen > longest
+          evalues << {:e => e, :length => best_hit_2.alnlen}
+        else
+          missed[best_hit_1.query] = best_hit_1
+          missed_evalues << {:e => e, :length => best_hit_2.alnlen}
+        end
+      end
+    end
+
+    
+
+  end
+
 end
 
---
+++ lib/hit.rb
@@ -0,0 +1,22 @@
+class Hit
+  # Fields: query id, subject id, % identity, alignment length, mismatches,
+  # gap opens, q. start, q. end, s. start, s. end, evalue, bit score
+  attr_accessor :query, :target, :id, :alnlen, :mismatches, :gaps, :qstart, :qend, :tstart, :tend, :evalue, :bitscore
+  def initialize(list)
+    @query      = list[0].split(/[\|\ ]/).first
+    @target     = list[1].split(/[\|\ ]/).first
+    @id         = list[2]
+    @alnlen     = list[3].to_i
+    @mismatches = list[4].to_i
+    @gaps       = list[5].to_i
+    @qstart     = list[6].to_i
+    @qend       = list[7].to_i
+    @tstart     = list[8].to_i
+    @tend       = list[9].to_i
+    @evalue     = list[10].to_f
+    @bitscore   = list[11].to_f
+  end
+  def to_s
+    return "#{@query}\t#{@target}\t#{@id}\t#{@alnlen}\t#{@evalue}\t#{@bitscore}\ttype:#{@type}\tpath:#{@path}"
+  end
+end
