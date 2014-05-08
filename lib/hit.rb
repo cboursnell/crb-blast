bddcb56dc8c8d88072905d472b8d6b7efdb8cbcc
Chris Boursnell <cboursnell@gmail.com>
2014-05-08 12:32:14 +0100

    added extra columns to blast output

--- lib/hit.rb
+++ lib/hit.rb
@@ -2,7 +2,7 @@ class Hit
   # Fields: query id, subject id, % identity, alignment length, mismatches,
   # gap opens, q. start, q. end, s. start, s. end, evalue, bit score
   attr_accessor :query, :target, :id, :alnlen, :mismatches, :gaps, :qstart, 
-  :qend, :tstart, :tend, :evalue, :bitscore
+  :qend, :tstart, :tend, :evalue, :bitscore, :qlen, :tlen
   
   def initialize(list)
     @query      = list[0].split(/[\|\ ]/).first
@@ -10,16 +10,20 @@ class Hit
     @id         = list[2]
     @alnlen     = list[3].to_i
     @mismatches = list[4].to_i
-    @gaps       = list[5].to_i
-    @qstart     = list[6].to_i
-    @qend       = list[7].to_i
-    @tstart     = list[8].to_i
-    @tend       = list[9].to_i
-    @evalue     = list[10].to_f
-    @bitscore   = list[11].to_f
+    # @gaps       = list[5].to_i
+    @qstart     = list[5].to_i
+    @qend       = list[6].to_i
+    @tstart     = list[7].to_i
+    @tend       = list[8].to_i
+    @evalue     = list[9].to_f
+    @bitscore   = list[10].to_f
+    @qlen       = list[11].to_f
+    @tlen       = list[12].to_f
   end
 
   def to_s
-    return "#{@query}\t#{@target}\t#{@id}\t#{@alnlen}\t#{@evalue}\t#{@bitscore}"
+    s = "#{@query}\t#{@target}\t#{@id}\t#{@alnlen}\t#{@evalue}\t#{@bitscore}\t"
+    s << "#{@qstart}..#{@qend}\t#{@tstart}..#{@tend}"
+    return s
   end
 end
