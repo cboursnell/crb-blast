226df4724049b1626d208fda3cc2416ad11ec153
Chris Boursnell <cboursnell@gmail.com>
2014-05-09 12:18:01 +0100

    moved the columns again

--- lib/hit.rb
+++ lib/hit.rb
@@ -5,20 +5,21 @@ class Hit
   :qend, :tstart, :tend, :evalue, :bitscore, :qlen, :tlen
   
   def initialize(list)
+    raise(RuntimeError, "unexpected number of columns") unless list.length==14
     @query      = list[0].split(/[\|\ ]/).first
     @target     = list[1].split(/[\|\ ]/).first
     @id         = list[2]
     @alnlen     = list[3].to_i
     @mismatches = list[4].to_i
-    # @gaps       = list[5].to_i
-    @qstart     = list[5].to_i
-    @qend       = list[6].to_i
-    @tstart     = list[7].to_i
-    @tend       = list[8].to_i
-    @evalue     = list[9].to_f
-    @bitscore   = list[10].to_f
-    @qlen       = list[11].to_f
-    @tlen       = list[12].to_f
+    @gaps       = list[5].to_i
+    @qstart     = list[6].to_i
+    @qend       = list[7].to_i
+    @tstart     = list[8].to_i
+    @tend       = list[9].to_i
+    @evalue     = list[10].to_f
+    @bitscore   = list[11].to_f
+    @qlen       = list[12].to_f
+    @tlen       = list[13].to_f
   end
 
   def to_s
