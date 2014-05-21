66a0b6b8abe7ca1d8371f898e98340209ef7503e
Chris Boursnell <cboursnell@gmail.com>
2014-05-21 10:40:14 +0100

    refactoring finding reciprocals

--- lib/crb-blast.rb
+++ lib/crb-blast.rb
@@ -22,7 +22,7 @@ class CRB_Blast
   attr_accessor :query_name, :target_name, :reciprocals
   attr_accessor :missed
   attr_accessor :target_is_prot, :query_is_prot
-  attr_accessor :query_results, :target_results
+  attr_accessor :query_results, :target_results, :working_dir
 
   def initialize query, target, output:nil
     @query = query
@@ -97,7 +97,9 @@ class CRB_Blast
     make_query_db_cmd << " -dbtype prot " if @query_is_prot
     make_query_db_cmd << " -title #{query_name} "
     make_query_db_cmd << " -out #{@working_dir}/#{query_name}"
-    if !File.exists?("#{@working_dir}/#{query_name}.nin")
+    db_query = "#{query_name}.nsq" if !@query_is_prot
+    db_query = "#{query_name}.psq" if @query_is_prot
+    if !File.exists?("#{@working_dir}/#{db_query}")
       `#{make_query_db_cmd}`
     end
 
@@ -164,66 +166,74 @@ class CRB_Blast
   end
 
   def load_outputs
-    @query_results = Hash.new
-    @target_results = Hash.new
-    q_count=0
-    t_count=0
-    if !File.exists?("#{@output1}")
-      puts "can't find #{@output1}"
-    end
-    if !File.exists?("#{@output2}")
-      puts "can't find #{@output2}"
-    end
-    if File.exists?("#{@output1}") and File.exists?("#{@output2}")
-      File.open("#{@output1}").each_line do |line|
-        cols = line.chomp.split("\t")
-        hit = Hit.new(cols)
-        @query_results[hit.query] = [] if !@query_results.has_key?(hit.query)
-        @query_results[hit.query] << hit
-        q_count += 1
-      end
-      File.open("#{@output2}").each_line do |line|
-        cols = line.chomp.split("\t")
-        hit = Hit.new(cols)
-        @target_results[hit.query] = [] if !@target_results.has_key?(hit.query)
-        @target_results[hit.query] << hit
-        t_count += 1
-      end
+    if File.exist?("#{@working_dir}/reciprocal_hits.txt")
+      # puts "reciprocal output already exists"
     else
-      raise "need to run blast first"
+      @query_results = Hash.new
+      @target_results = Hash.new
+      q_count=0
+      t_count=0
+      if !File.exists?("#{@output1}")
+        puts "can't find #{@output1}"
+      end
+      if !File.exists?("#{@output2}")
+        puts "can't find #{@output2}"
+      end
+      if File.exists?("#{@output1}") and File.exists?("#{@output2}")
+        File.open("#{@output1}").each_line do |line|
+          cols = line.chomp.split("\t")
+          hit = Hit.new(cols)
+          @query_results[hit.query] = [] if !@query_results.has_key?(hit.query)
+          @query_results[hit.query] << hit
+          q_count += 1
+        end
+        File.open("#{@output2}").each_line do |line|
+          cols = line.chomp.split("\t")
+          hit = Hit.new(cols)
+          @target_results[hit.query] = [] if !@target_results.has_key?(hit.query)
+          @target_results[hit.query] << hit
+          t_count += 1
+        end
+      else
+        raise "need to run blast first"
+      end
     end
     [q_count, t_count]
   end
 
   # fills @reciprocals with strict reciprocal hits from the blast results
   def find_reciprocals
-    @reciprocals = Hash.new
-    @missed = Hash.new
-    @evalues = []
-    @longest=0
-    hits = 0
-    @query_results.each_pair do |query_id, list_of_hits|
-      list_of_hits.each_with_index do |target_hit, query_index|
-        if @target_results.has_key?(target_hit.target)
-          list_of_hits_2 = @target_results[target_hit.target]
-          list_of_hits_2.each_with_index do |query_hit2, target_index|
-            if query_index == 0 && target_index == 0 &&
-               query_id == query_hit2.target
-              e = target_hit.evalue.to_f
-              e = 1e-200 if e==0
-              e = -Math.log10(e)
-              if !@reciprocals.key?(query_id)
-                @reciprocals[query_id] = []
+    if File.exist?("#{@working_dir}/reciprocal_hits.txt")
+      # puts "reciprocal output already exists"
+    else
+      @reciprocals = Hash.new
+      @missed = Hash.new
+      @evalues = []
+      @longest=0
+      hits = 0
+      @query_results.each_pair do |query_id, list_of_hits|
+        list_of_hits.each_with_index do |target_hit, query_index|
+          if @target_results.has_key?(target_hit.target)
+            list_of_hits_2 = @target_results[target_hit.target]
+            list_of_hits_2.each_with_index do |query_hit2, target_index|
+              if query_index == 0 && target_index == 0 &&
+                 query_id == query_hit2.target
+                e = target_hit.evalue.to_f
+                e = 1e-200 if e==0
+                e = -Math.log10(e)
+                if !@reciprocals.key?(query_id)
+                  @reciprocals[query_id] = []
+                end
+                @reciprocals[query_id] << target_hit
+                hits += 1
+                @longest = target_hit.alnlen  if target_hit.alnlen > @longest
+                @evalues << {:e => e, :length => target_hit.alnlen}
+              elsif query_id == query_hit2.target
+                if !@missed.key?(query_id)
+                  @missed[query_id] = []
+                end
+                @missed[query_id] << target_hit
               end
-              @reciprocals[query_id] << target_hit
-              hits += 1
-              @longest = target_hit.alnlen  if target_hit.alnlen > @longest
-              @evalues << {:e => e, :length => target_hit.alnlen}
-            elsif query_id == query_hit2.target
-              if !@missed.key?(query_id)
-                @missed[query_id] = []
-              end
-              @missed[query_id] << target_hit
             end
           end
         end
@@ -233,53 +243,58 @@ class CRB_Blast
   end
 
   def find_secondaries
-    length_hash = Hash.new
-    fitting = Hash.new
-    @evalues.each do |h|
-      length_hash[h[:length]] = [] if !length_hash.key?(h[:length])
-      length_hash[h[:length]] << h
-    end
 
-    (10..@longest).each do |centre|
-      e = 0
-      count = 0
-      s = centre*0.1
-      s = s.to_i
-      s = 5 if s < 5
-      (-s..s).each do |side|
-        if length_hash.has_key?(centre+side)
-          length_hash[centre+side].each do |point|
-            e += point[:e]
-            count += 1
+    if File.exist?("#{@working_dir}/reciprocal_hits.txt")
+      # puts "reciprocal output already exists"
+    else
+      length_hash = Hash.new
+      fitting = Hash.new
+      @evalues.each do |h|
+        length_hash[h[:length]] = [] if !length_hash.key?(h[:length])
+        length_hash[h[:length]] << h
+      end
+
+      (10..@longest).each do |centre|
+        e = 0
+        count = 0
+        s = centre*0.1
+        s = s.to_i
+        s = 5 if s < 5
+        (-s..s).each do |side|
+          if length_hash.has_key?(centre+side)
+            length_hash[centre+side].each do |point|
+              e += point[:e]
+              count += 1
+            end
           end
         end
+        if count>0
+          mean = e/count
+          fitting[centre] = mean
+        end
       end
-      if count>0
-        mean = e/count
-        fitting[centre] = mean
-      end
-    end
-    hits = 0
-    @missed.each_pair do |id, list|
-      list.each do |hit|
-        l = hit.alnlen.to_i
-        e = hit.evalue
-        e = 1e-200 if e==0
-        e = -Math.log10(e)
-        if fitting.has_key?(l)
-          if e >= fitting[l]
-            if !@reciprocals.key?(id)
-              @reciprocals[id] = []
-              found=false
-              @reciprocals[id].each do |existing_hit|
-                if existing_hit.query == hit.query &&
-                  existing_hit.target == hit.target
-                 found=true
+      hits = 0
+      @missed.each_pair do |id, list|
+        list.each do |hit|
+          l = hit.alnlen.to_i
+          e = hit.evalue
+          e = 1e-200 if e==0
+          e = -Math.log10(e)
+          if fitting.has_key?(l)
+            if e >= fitting[l]
+              if !@reciprocals.key?(id)
+                @reciprocals[id] = []
+                found=false
+                @reciprocals[id].each do |existing_hit|
+                  if existing_hit.query == hit.query &&
+                    existing_hit.target == hit.target
+                   found=true
+                  end
                 end
-              end
-              if !found
-                @reciprocals[id] << hit
-                hits += 1
+                if !found
+                  @reciprocals[id] << hit
+                  hits += 1
+                end
               end
             end
           end
@@ -287,6 +302,13 @@ class CRB_Blast
       end
     end
     return hits
+  end
+
+  def clear_memory
+    # running lots of jobs at the same time was keeping a lot of stuff in
+    # memory that you might not want so this empties out those big hashes.
+    @query_results = nil
+    @target_results = nil
   end
 
   def run evalue, threads
